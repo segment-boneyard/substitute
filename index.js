b83f67807123a894fd220fffa2f19029f1d21af5
Ian Storm Taylor <ian@ianstormtaylor.com>
2014-08-14 16:52:44 -0700

    add array support

--- index.js
+++ index.js
@@ -6,10 +6,16 @@
 module.exports = substitute;
 
 /**
+ * Type.
+ */
+
+var type = Object.prototype.toString;
+
+/**
  * Substitute `:prop` with the given `obj` in `str`
  *
  * @param {String} str
- * @param {Object} obj
+ * @param {Object or Array} obj
  * @param {RegExp} expr
  * @return {String}
  * @api public
@@ -19,8 +25,12 @@ function substitute(str, obj, expr){
   if (!obj) throw new TypeError('expected an object');
   expr = expr || /:(\w+)/g;
   return str.replace(expr, function(_, prop){
-    return null != obj[prop]
-      ? obj[prop]
-      : _;
+    switch (type.call(obj)) {
+      case '[object Object]':
+        return null != obj[prop] ? obj[prop] : _;
+      case '[object Array]':
+        var val = obj.shift();
+        return null != val ? val : _;
+    }
   });
 }
